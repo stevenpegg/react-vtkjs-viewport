f0179690ae0524f9d7c01af7d07a9524db27c427
Steven Pegg <steven@adapptor.com.au>
2021-03-10 11:59:50 +0800

    Remove any slices that have the same position as their neighbour

--- src/lib/data/sortDatasetsByImagePosition.js
+++ src/lib/data/sortDatasetsByImagePosition.js
@@ -11,14 +11,14 @@ export default function sortDatasetsByImagePosition(
 ) {
   // See https://github.com/dcmjs-org/dcmjs/blob/4849ed50db8788741c2773b3d9c75cc52441dbcb/src/normalizers.js#L167
   // TODO: Find a way to make this code generic?
-  console.log('imageMetaDataMap');
-  console.log(imageMetaDataMap);
+  // console.log('imageMetaDataMap');
+  // console.log(imageMetaDataMap);
 
   const datasets = Array.from(imageMetaDataMap.values());
   const referenceDataset = datasets[0];
 
-  console.log('datasets');
-  console.log(datasets);
+  // console.log('datasets');
+  // console.log(datasets);
 
   const distanceDatasetPairs = datasets.map(function(dataset) {
     const positionVector = vec3.sub(
@@ -41,15 +41,16 @@ export default function sortDatasetsByImagePosition(
   const sortedDatasets = distanceDatasetPairs.map(a => a.dataset);
   const distances = distanceDatasetPairs.map(a => a.distance);
 
-  console.log('sortDatasetsByImagePosition distances');
-  console.log(distances[0]);
-  console.log(distances[0]);
-  console.log('all distances', distances);
-  for (let i = 0; i < distances.length - 1; i++) {
-    console.log('distance', i, Math.abs(distances[i + 1] - distances[i]));
-  }
+  // console.log('sortDatasetsByImagePosition distances');
+  // console.log(distances[0]);
+  // console.log(distances[0]);
+  // console.log('all distances', distances);
+  // for (let i = 0; i < distances.length - 1; i++) {
+  //  console.log('distance', i, Math.abs(distances[i + 1] - distances[i]));
+  // }
 
   // Remove any slices that share the same position as the next slice.
+  /*
   for (let i = distances.length - 1; i > 0; i--) {
     if (Math.abs(distances[i - 1] - distances[i]) <= 0.0001) {
       console.log('Removing slice', i);
@@ -57,15 +58,17 @@ export default function sortDatasetsByImagePosition(
       distances.splice(i, 1);
     }
   }
-  console.log('There are now', distances.length, 'slices');
+  */
+  // console.log('There are now', distances.length, 'slices');
 
   // TODO: The way we calculate spacing determines how the volume shows up if
   // we have missing slices.
   // - Should we just bail out for now if missing slices are present?
-  //const spacing = mean(diff(distances));
-  const spacing = Math.abs(distances[1] - distances[0]);
-  console.log('sortDatasetsByImagePosition spacing');
-  console.log(spacing);
+  // const spacing = mean(diff(distances));
+  let spacing = Math.abs(distances[1] - distances[0]);
+  console.log('sortDatasetsByImagePosition spacing', spacing);
+  spacing = mean(diff(distances));
+  console.log('sortDatasetsByImagePosition spacing NOW', spacing);
 
   return {
     spacing,
